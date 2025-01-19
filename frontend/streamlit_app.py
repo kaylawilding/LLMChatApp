28b59d82763f9dba0d175a7f9bcdd7658282490d
kaylawilding <95330483+kaylawilding@users.noreply.github.com>
2025-01-18 18:04:45 -0800

    Add Streamlit app

---
+++ frontend/streamlit_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import requests
+import os
+import pandas as pd
+from openai import OpenAI
+
+BACKEND_URL = os.getenv('BACKEND_URL')
+
+st.title('LLM Chat')
+
+with st.form('LoanApp'):
+    gender = st.selectbox(
+    'Select a Model',
+        ('Female', 'Male', 'Joint'))
+
+st.title("ChatGPT-like clone")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("How can I help?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
